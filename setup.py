be8ee1c943a2bfeeeb10010c5d994e149b57b06a
Max Arnold <arnold.maxim@gmail.com>
2013-06-21 13:26:31 +0700

    bump version to 0.1.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from distutils.core import setup
 
 __author__ = 'Max Arnold <arnold.maxim@gmail.com>'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 setup(
     name='fetch-google-reader',
